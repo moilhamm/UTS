73644e1596c75cacacc8edcfa47c49acdc79f963
moilhamm <mohh.ilhamm@gmail.com>
2026-04-24 14:04:19 +0700

    push

--- InputMahasiswa.py
+++ InputMahasiswa.py
@@ -5,3 +5,12 @@ ipk = int(input("Masukan IPK : "))
 print(f"Nama Mahasiswa : {nama_mahasiswa}")
 print(f"Umur Mahasiswa : {umur}")
 print(f"Ipk Mahasiswa : {ipk}")
+
+''' Variabel adalah tempat untuk menyimpan data dalam program,
+    yang nilainya bisa berubah-ubah. Sedangkan tipe data
+    adalah jenis dari data yang disimpan di dalam variabel tersebut.
+    String adalah tipe data yang berisi teks atau karakter.
+    Integer adalah bilangan bulat tanpa desimal.
+    Float adalah bilangan yang memiliki desimal.
+    Boolean adalah tipe data yang hanya memiliki dua nilai,
+    yaitu benar (True) dan salah (False)'''
